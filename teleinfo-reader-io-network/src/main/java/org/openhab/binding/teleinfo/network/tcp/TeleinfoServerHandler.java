54fb777158f9af59dbe84f1febb5a683df17b4fe
nsiberil <nokyyz@yahoo.fr>
2016-01-13 15:32:20 +0100

    Remove TeleinfoReaderListener implementation on TeleinfoServerHandler

--- teleinfo-reader-io-network/src/main/java/org/openhab/binding/teleinfo/network/tcp/TeleinfoServerHandler.java
+++ teleinfo-reader-io-network/src/main/java/org/openhab/binding/teleinfo/network/tcp/TeleinfoServerHandler.java
@@ -3,13 +3,10 @@ package org.openhab.binding.teleinfo.network.tcp;
 import org.apache.mina.core.service.IoHandler;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
-import org.openhab.binding.teleinfo.reader.Frame;
-import org.openhab.binding.teleinfo.reader.io.TeleinfoReader;
-import org.openhab.binding.teleinfo.reader.io.TeleinfoReaderListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TeleinfoServerHandler implements IoHandler, TeleinfoReaderListener {
+public class TeleinfoServerHandler implements IoHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TeleinfoServerHandler.class);
 
@@ -52,10 +49,5 @@ public class TeleinfoServerHandler implements IoHandler, TeleinfoReaderListener 
     public void sessionOpened(IoSession session) throws Exception {
         // NOP
         LOGGER.info("Total opened sessions on server: {}", session.getService().getManagedSessionCount());
-    }
-
-    @Override
-    public void onFrameReceived(final TeleinfoReader reader, final Frame frame) {
-        // NOP
     }
 }
