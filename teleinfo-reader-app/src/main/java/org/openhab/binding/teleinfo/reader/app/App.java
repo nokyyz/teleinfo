2b4dc432de4233d3584578be6715b748e9449eff
nsiberil <nokyyz@yahoo.fr>
2016-02-13 18:34:36 +0100

    Add options to install daemon service

--- teleinfo-reader-app/src/main/java/org/openhab/binding/teleinfo/reader/app/App.java
+++ teleinfo-reader-app/src/main/java/org/openhab/binding/teleinfo/reader/app/App.java
@@ -2,6 +2,22 @@ package org.openhab.binding.teleinfo.reader.app;
 
 import static org.openhab.binding.teleinfo.reader.io.serialport.TeleinfoInputStream.*;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.openhab.binding.teleinfo.network.tcp.TeleinfoServer;
@@ -15,6 +31,7 @@ import org.slf4j.LoggerFactory;
 public class App {
 
     private static Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static final String DAEMON_NAME = "teleinfo-deamon";
 
     private Options options;
 
@@ -70,19 +87,33 @@ public class App {
 
             final App app = new App(options);
 
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        app.stop();
-                        // exit(ReturnCode.SUCCESS_RETURN_CODE);
-                    } catch (Exception e) {
-                        LOGGER.warn("An error during the application shutdown", e);
-                    }
-                }
-            });
+            if (options.isInstallAsService()) {
+                System.out.println("Installing Teleinfo Reader service...");
+                installAsService(options, args);
+                System.out.println("Teleinfo Reader service installed !");
+                System.out.println("To start automatically this service, execute the following commands:");
+                System.out.println("sudo chmod a+x /etc/init.d/" + DAEMON_NAME);
+                System.out.println("sudo update-rc.d " + DAEMON_NAME + " defaults");
+                System.out.println();
 
-            app.start();
+                System.out.println("Bye");
+                exit(ReturnCode.SUCCESS_RETURN_CODE);
+            } else {
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            app.stop();
+                            // exit(ReturnCode.SUCCESS_RETURN_CODE);
+                        } catch (Exception e) {
+                            LOGGER.warn("An error during the application shutdown", e);
+                        }
+                    }
+                });
+
+                LOGGER.debug("Args: " + args);
+                app.start();
+            }
         } catch (final CmdLineException e) {
             displayHelp(parser);
             exit(ReturnCode.ERROR_RETURN_CODE);
@@ -95,7 +126,9 @@ public class App {
 
     private static void displayHelp(final CmdLineParser parser) {
         parser.printUsage(System.err);
-        System.err.println("Example: ./start.sh -serialPort /dev/ttyS80 -refreshInterval 4000");
+        System.err.println("Example 1: ./start.sh -serialPort /dev/ttyS80 -refreshInterval 4000");
+        System.err.println(
+                "Example 2: ./start.sh -serialPort /dev/ttyS80 -refreshInterval 4000 -installAsService -serviceRunAs root -useSerialPortSymbolicLink -serialPortSymbolicLinkTarget /dev/ttyAMA0");
         System.err.println(
                 "Tips: sometimes the serial port can require a symbolic link (e.g: 'sudo ln -s /dev/ttyAMA0 /dev/ttyS80')");
     }
@@ -115,5 +148,81 @@ public class App {
         }
 
         System.exit(exit);
+    }
+
+    private static void installAsService(final Options options, String[] appArgs) throws Exception {
+        String currentUser = System.getProperty("user.name");
+        if (!"root".equals(currentUser)) {
+            throw new IllegalStateException("To install service, you must be logged with 'root' user");
+        }
+
+        String osName = System.getProperty("os.name");
+        if (!osName.contains("nix") && !osName.contains("nux") && !osName.contains("aix")) {
+            throw new IllegalStateException("'Install service' option is only on Linux");
+        }
+
+        Path serviceScriptFile = Paths.get("/etc/init.d/" + DAEMON_NAME);
+        if (Files.exists(serviceScriptFile)) {
+            throw new IllegalStateException("Service already exists. To reinstall service, remove '" + DAEMON_NAME
+                    + "' file (" + serviceScriptFile.toAbsolutePath().toString() + ")");
+        }
+
+        Map<String, String> variables = new HashMap<>();
+        variables.put("@@APP_HOME@@", new File("").getAbsolutePath());
+        StringBuilder argsWithSpace = new StringBuilder();
+        // suppress '-installAsService' option
+        for (String appArg : appArgs) {
+            if (!"-installAsService".equalsIgnoreCase(appArg) && !"-serviceRunAs".equalsIgnoreCase(appArg)
+                    && !options.getServiceRunAs().equalsIgnoreCase(appArg)
+                    && !"-useSerialPortSymbolicLink".equalsIgnoreCase(appArg)
+                    && !"-serialPortSymbolicLinkTarget".equalsIgnoreCase(appArg)
+                    && !options.getSerialPortSymbolicLinkTarget().equalsIgnoreCase(appArg)) {
+                argsWithSpace.append(appArg);
+                argsWithSpace.append(" ");
+            }
+        }
+        variables.put("@@DAEMON_ARGS@@", argsWithSpace.toString());
+        variables.put("@@RUN_AS@@", options.getServiceRunAs());
+        if (options.isUseSerialPortSymbolicLink()) {
+            variables.put("@@USE_SERIAL_PORT_SYMBOLIC_LINK@@", "1");
+            variables.put("@@SERIAL_PORT_PATH@@", options.getSerialPortSymbolicLinkTarget());
+            variables.put("@@SERIAL_PORT_SYMBOLIC_LINK_PATH@@", options.getSerialPort());
+        } else {
+            variables.put("@@USE_SERIAL_PORT_SYMBOLIC_LINK@@", "0");
+            variables.put("@@SERIAL_PORT_PATH@@", "");
+            variables.put("@@SERIAL_PORT_SYMBOLIC_LINK_PATH@@", "");
+        }
+
+        final Charset utf8Charset = Charset.forName("UTF-8");
+
+        try (BufferedReader templateScriptReader = new BufferedReader(new InputStreamReader(
+                App.class.getClassLoader().getResourceAsStream("misc/linux/install-as-service-template.sh"),
+                utf8Charset));
+                BufferedWriter scriptWriter = Files.newBufferedWriter(serviceScriptFile, utf8Charset,
+                        StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW);) {
+            String line = null;
+            while ((line = templateScriptReader.readLine()) != null) {
+                for (Entry<String, String> variableEntry : variables.entrySet()) {
+                    // line = line.replaceAll(variableEntry.getKey(), variableEntry.getValue());
+                    line = line.replace(variableEntry.getKey(), variableEntry.getValue());
+                }
+
+                scriptWriter.write(line);
+                scriptWriter.newLine();
+            }
+
+        }
+
+        // using PosixFilePermission to set file permissions 777
+        Set<PosixFilePermission> perms = new HashSet<PosixFilePermission>();
+        perms.add(PosixFilePermission.OWNER_READ);
+        perms.add(PosixFilePermission.OWNER_WRITE);
+        perms.add(PosixFilePermission.OWNER_EXECUTE);
+        perms.add(PosixFilePermission.GROUP_READ);
+        perms.add(PosixFilePermission.GROUP_EXECUTE);
+        perms.add(PosixFilePermission.OTHERS_READ);
+        perms.add(PosixFilePermission.OTHERS_EXECUTE);
+
+        Files.setPosixFilePermissions(serviceScriptFile, perms);
     }
 }
--- teleinfo-reader-app/src/main/java/org/openhab/binding/teleinfo/reader/app/Options.java
+++ teleinfo-reader-app/src/main/java/org/openhab/binding/teleinfo/reader/app/Options.java
@@ -10,6 +10,18 @@ public class Options {
     @Option(name = "-refreshInterval", required = true, usage = "The frequency between 2 data read (in millisecond)")
     private long refreshInterval;
 
+    @Option(name = "-installAsService", required = false, usage = "Install app as a service (only Linux os)", depends = "-serviceRunAs")
+    private boolean installAsService;
+
+    @Option(name = "-serviceRunAs", usage = "User used by Service app (only Linux os)")
+    private String serviceRunAs;
+
+    @Option(name = "-useSerialPortSymbolicLink", required = false, usage = "Enable the serial port symbolic link creation", depends = "-serialPortSymbolicLinkTarget")
+    private boolean useSerialPortSymbolicLink;
+
+    @Option(name = "-serialPortSymbolicLinkTarget", usage = "Path of target serial port")
+    private String serialPortSymbolicLinkTarget;
+
     public String getSerialPort() {
         return serialPort;
     }
@@ -18,4 +30,19 @@ public class Options {
         return refreshInterval;
     }
 
+    public boolean isInstallAsService() {
+        return installAsService;
+    }
+
+    public String getServiceRunAs() {
+        return serviceRunAs;
+    }
+
+    public boolean isUseSerialPortSymbolicLink() {
+        return useSerialPortSymbolicLink;
+    }
+
+    public String getSerialPortSymbolicLinkTarget() {
+        return serialPortSymbolicLinkTarget;
+    }
 }
